09387f488e5895529ea36a2f74883863643d42f9
kfir <kfir.k4@gmail.com>
2022-07-21 14:04:25 +0300

    initial commit

---
+++ install_2.sh
@@ -0,0 +1,35 @@
+# after running install_1.sh
+
+# Source .bashrc
+source ~/.bashrc
+
+# Install mamba (https://github.com/mamba-org/mamba, https://mamba.readthedocs.io/en/latest/user_guide/mamba.html)
+conda install mamba -n base -c conda-forge -y
+
+#install psi4
+mamba install -c psi4 psi4
+mamba create -n p4env psi4 -c psi4
+
+# Clone repos
+cd /home/$USER/Code
+git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
+git clone https://github.com/ReactionMechanismGenerator/ARC.git
+git clone https://github.com/ReactionMechanismGenerator/T3.git
+
+# Copy dummy script
+cp /home/$USER/Code/ARC/ipython/Tools/conformers.ipynb /home/$USER/Code/scripts/conformers.ipynb
+
+# Create envs (and rename remote as official for consistency)
+cd RMG-database
+git remote rename origin official
+cd ../ARC
+git remote rename origin official
+mamba env create -f environment.yml
+cd ../T3
+git remote rename origin official
+mamba env create -f environment.yml
+cd ../RMG-Py
+git remote rename origin official
+mamba env create -f environment.yml
+
---
+++ install_3.sh
@@ -0,0 +1,12 @@
+# This script should be used when creating a new account for a user on our machines
+# after running install_2.sh and manually activating rmg_env
+
+
+conda activate rmg_env
+cd ~/Code/RMG-Py/
+make
+python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
+julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",version="0.4")); using ReactionMechanismSimulator;'
+make test
+conda deactivate
+
