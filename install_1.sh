45a6c8d35fbb87b25e5f3bf1e4d0402db57aa94b
kfir4444 <kfir.k4@gmail.com>
2022-08-02 11:40:04 +0300

    conda fixups

--- install_1.sh
+++ install_1.sh
@@ -1,4 +1,5 @@
 # Make sure periodically that we're downloading the most up to date Anaconda version (see https://www.anaconda.com/products/individual, bottom right)
+cd ..
 
 # apt install
 sudo apt install git gcc g++ make -y
@@ -11,5 +12,5 @@ mkdir /home/$USER/runs
 wget -P /home/$USER/Downloads https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
 
 # Installation
-bash /home/$USER/Downloads/Anaconda3-2021.11-Linux-x86_64.sh
-
+bash /home/$USER/Downloads/Anaconda3-2021.11-Linux-x86_64.sh -u
+export path="~/anaconda3/bin/conda"
